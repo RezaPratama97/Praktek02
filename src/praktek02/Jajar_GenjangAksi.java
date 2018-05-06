cf748e3288409963788c8c3e4219fbfabf2b857c
Muhammad Reza Rizky Pratama <muhammadrezarizkypratama@gmail.com>
2018-05-06 10:03:29 +0700

    Mengisi nilai atribute

--- src/praktek02/Jajar_GenjangAksi.java
+++ src/praktek02/Jajar_GenjangAksi.java
@@ -3,5 +3,7 @@ package praktek02;
 public class Jajar_GenjangAksi {
     public static void main(String[] args) {
         Jajar_Genjang rl = new Jajar_Genjang();
+        rl.alas   = 7;
+        rl.tinggi = 9;
     }
 }

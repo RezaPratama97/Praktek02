d741e45d6430e448ca49f35bc2416b903608f0a0
Muhammad Reza Rizky Pratama <muhammadrezarizkypratama@gmail.com>
2018-05-06 09:55:41 +0700

    Membuat Attribute dan Behavior

--- src/praktek02/Jajar_Genjang.java
+++ src/praktek02/Jajar_Genjang.java
@@ -1,4 +1,12 @@
 package praktek02;
 public class Jajar_Genjang {
+    double alas;
+    double tinggi;
     
+    void cetakInfo() {
+        System.out.println("==================");
+        System.out.println("alas   :"+alas);
+        System.out.println("tinggi :"+tinggi);
+        System.out.println("==================");
+    }
 }
